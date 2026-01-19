de57b6cbffdfd57ca6d83a357985517f95acf069
willAtaides <sg552.santos@gmail.com>
2026-01-19 05:56:20 -0300

    h2 database, repositorie, service

--- backend/src/main/java/com/devsuperior/dscatalog/entities/Category.java
+++ backend/src/main/java/com/devsuperior/dscatalog/entities/Category.java
@@ -1,10 +1,16 @@
 package com.devsuperior.dscatalog.entities;
 
+import jakarta.persistence.*;
+
 import java.io.Serializable;
 import java.util.Objects;
 
+@Entity
+@Table(name = "tb_category")
 public class Category implements Serializable {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
 
